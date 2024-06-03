643c1458dc10e95c226f04969300612030d3f370
Parikesittt <parikesit.widodo@gmail.com>
2024-06-03 11:43:56 +0700

    update

--- app/src/main/java/com/example/testtubesmanual/EditWisataActivity.kt
+++ app/src/main/java/com/example/testtubesmanual/EditWisataActivity.kt
@@ -1,29 +1,20 @@
 package com.example.testtubesmanual
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class WelcomeActivity : AppCompatActivity() {
+class EditWisataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_edit_wisata)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val button = findViewById<Button>(R.id.buttonLanjut)
-
-        button.setOnClickListener {
-            Intent(this@WelcomeActivity,RegisterActivity::class.java).also {
-                startActivity(it)
-            }
         }
     }
 }
